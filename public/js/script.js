b2dd46608acd39288c7aca92d201b591291d23ce
Shikuku39 <enhikuku39@gmail.com>
2023-01-13 01:12:49 +0300

    added eventlisteners

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,19 @@ const load = document.querySelector(".loader")
 window.addEventListener("load", function(){
     load.style.display = 'none'
 })
+
+let students = document.querySelectorAll('.studentidandname')
+let details = document.querySelectorAll('.studentdetailedinformation')
+
+students.forEach(student => {
+    student.addEventListener('click', () => {
+        let detail = student.nextElementSibling
+        if (detail.style.display === 'none') {
+            details.forEach(detail => detail.style.display = 'none')
+            detail.style.display = 'flex'
+            
+        } else {
+            detail.style.display = 'none'
+        }
+    })
+})
