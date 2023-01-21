c49b0858a3e226db7a7e0eaec95babb6ecfd35d4
Shikuku39 <enhikuku39@gmail.com>
2023-01-21 11:41:02 +0300

    added scroll to bottom button

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 const load = document.querySelector(".loader")
 let students = document.querySelectorAll('.studentidandname')
 let details = document.querySelectorAll('.studentdetailedinformation')
+var button = document.querySelector('#scroll-to-bottom-button')
 
 window.addEventListener("load", function(){
     load.style.display = 'none'
@@ -18,3 +19,27 @@ students.forEach(student => {
         }
     })
 })
+button.addEventListener("click", function() {
+    var currentHeight = document.body.scrollHeight
+    window.scrollTo(0, currentHeight)
+})
+
+var button = document.getElementById("scroll-to-bottom-button")
+button.addEventListener("click", function() {
+    window.scroll({
+        top: document.body.scrollHeight,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
+
+function showHideButton() {
+    var currentHeight = document.body.scrollHeight
+    if (currentHeight > window.innerHeight) {
+        button.style.display = "block"
+    } else {
+        button.style.display = "none"
+    }
+}
+showHideButton()
+window.addEventListener("resize", showHideButton)
